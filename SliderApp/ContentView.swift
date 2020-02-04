ee41cfe9bf1c76af790ced8ea5cf524f8a294818
ohnezahn <supidupi12@yahoo.de>
2020-02-04 19:00:00 +0100

    Initial Commit

---
+++ SliderApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SliderApp
+//
+//  Created by Benjamin Müller on 04.02.20.
+//  Copyright © 2020 Benjamin Müller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
